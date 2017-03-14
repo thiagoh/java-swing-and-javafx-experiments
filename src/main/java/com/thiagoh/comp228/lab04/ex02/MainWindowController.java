7b44e4f896b897452083470f8f7eb74ddbaa15d7
Thiago Andrade <thiagoh@gmail.com>
2017-03-14 19:52:57 -0400

    javafx working - gui partially

--- src/main/java/com/thiagoh/comp228/lab04/ex02/MainWindowController.java
+++ src/main/java/com/thiagoh/comp228/lab04/ex02/MainWindowController.java
@@ -13,17 +13,34 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
 public class MainWindowController implements Initializable {
+
+	@FXML
+	private RadioButton radioShapeRectangle;
+	@FXML
+	private RadioButton radioShapeCircle;
+	@FXML
+	private RadioButton radioShapeSquare;
+
+	@FXML
+	private CheckBox checkboxColorBackground;
+	@FXML
+	private CheckBox checkboxColorFill;
+	@FXML
+	private CheckBox checkboxColorLine;
 
 	@FXML
 	private TextField textFieldName;
 
 	@FXML
 	private ComboBox<String> comboboxLineColor;
+
 	@FXML
 	private ListView<String> listColorFill;
 
@@ -42,6 +59,55 @@ public class MainWindowController implements Initializable {
 		comboboxLineColor.setItems(FXCollections.observableArrayList(colorsName));
 
 		listColorFill.setItems(FXCollections.observableArrayList(colorsName));
+	}
+
+	public String getUserName() {
+		return textFieldName.getText();
+	}
+
+	public String getShapeType() {
+
+		String shapeType = null;
+
+		if (radioShapeRectangle.isSelected()) {
+			shapeType = "rectangle";
+		}
+
+		if (radioShapeSquare.isSelected()) {
+			shapeType = "square";
+		}
+
+		if (radioShapeCircle.isSelected()) {
+			shapeType = "circle";
+		}
+
+		return shapeType;
+	}
+
+	public Color getOutlineColor() {
+		int colorIndex = Math.max(0, comboboxLineColor.getSelectionModel().getSelectedIndex());
+		return colors[colorIndex];
+	}
+
+	public Color getFillColor() {
+		int colorIndex = Math.max(0, listColorFill.getSelectionModel().getSelectedIndex());
+		return colors[colorIndex];
+	}
+
+	public Color getBacklColor() {
+		return Color.white;
+	}
+
+	public boolean isShapeFilled() {
+		return checkboxColorFill.isSelected();
+	}
+
+	public boolean isShapeOutlined() {
+		return checkboxColorLine.isSelected();
+	}
+
+	public boolean isShapeBackColored() {
+		return checkboxColorBackground.isSelected();
 	}
 
 }
